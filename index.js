ffcf2ce23692965dd7bcf83cb9667236621d0a78
Germaine144 <umuhiregermaine12@gmail.com>
2025-03-28 09:44:08 +0200

    adding objects in array

---
+++ index.js
@@ -0,0 +1,13 @@
+// create a objects
+const products = [
+    { name: "Laptop", price: 800, category: "Electronics" },
+    { name: "Phone", price: 500, category: "Electronics" },
+    { name: "Table", price: 200, category: "Furniture" },
+    { name: "Chair", price: 100, category: "Furniture" },
+    { name: "Headphones", price: 50, category: "Electronics" },
+    { name: "Shoes", price: 70, category: "Clothing" },
+    { name: "Jacket", price: 120, category: "Clothing" },
+    { name: "Watch", price: 250, category: "Accessories" },
+    { name: "Backpack", price: 90, category: "Accessories" },
+    { name: "Sofa", price: 600, category: "Furniture" }
+];
